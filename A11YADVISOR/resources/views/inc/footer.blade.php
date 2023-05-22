aa0755ae1d6f1c8abf42c80e2994a98f904a74a2
MoeShoumar <mks38@mail.aub.edu>
2023-05-23 01:25:39 +0300

    create and import footer

---
+++ A11YADVISOR/resources/views/inc/footer.blade.php
@@ -0,0 +1,4 @@
+
+<footer class="footer">
+    <p class="footer-content">AAARDVARK All Rights Reserved 2023</p>
+</footer>
--- A11YADVISOR/resources/views/layouts/app.blade.php
+++ A11YADVISOR/resources/views/layouts/app.blade.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{asset('css/app.css')}} " rel="stylesheet">
+    <link href="{{asset('css/navBar_footer.css')}}" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{config('app.name')}}</title>
